04f585a462ee636d858d528cf2389e6f3abddb00
Nai-intellact <webdevfahimislam@gamil.com>
2023-11-19 10:13:32 +0600

    first commit

---
+++ html/js/index.js
@@ -0,0 +1,25 @@
+function loadCountries() {
+    fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+
+
+}
+const displayCountries = countries => {
+    const allCountryHTML = countries.map(country => getCountryHTML(country));
+    console.log(allCountryHTML);
+    const container = document.getElementById('countries');
+
+    container.innerHTML = allCountryHTML.join(' ');
+
+}
+const getCountryHTML = country => {
+    return `
+    <div class="country">
+     <h2> ${country.name.common}</h2>
+     <img src="${country.flags.png}" alt="this is the flag">
+    </div>
+    `
+}
+
+loadCountries();
